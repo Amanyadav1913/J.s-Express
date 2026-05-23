c74d20fe51943a21064e7bb78a476b2219894583
Aman yadav <amanku12090@gmail.com>
2026-05-23 20:49:10 +0530

    first commit

---
+++ operation.js
@@ -0,0 +1,55 @@
+// ************Operation************
+
+// let value = 3
+// let negValue =  -value
+// console.log(negValue) // -3
+
+// console.log(2 + 3) // 5
+// console.log(2 - 3) // -1
+// console.log(2 * 3) // 6
+// console.log(2 / 3) // 0.6666666666666666
+// console.log(2 % 3) // 2
+
+// let str1 = "Hello"
+// let str2 = "World"
+// let str3 = str1+" "+str2
+// console.log(str3) // Hello World
+
+// console.log("5" + 3) // 53
+// console.log("5" - 3) // 2
+ 
+
+// console.log("1" + 2 +3) // 123
+// console.log(1 + 2 + "3") // 33
+// console.log(3 + 4 * 5 % 3) // 5
+
+// console.log(true) // true
+// console.log(+'') // 0
+//  console.log(+true) // 1
+//  console.log(-true) // -1
+//  console.log(!true) // false
+
+// let gameCounter = 100
+// gameCounter++
+// gameCounter -- // 99
+// ++gameCounter // 101
+// console.log(gameCounter) // 101
+
+// ********Comparision Operators************
+
+// console.log(2 > 3) // false
+// console.log(2 < 3) // true
+// console.log(2 >= 3) // false
+// console.log(2 <= 3) // true
+// console.log(2 == 3) // false
+// console.log(2 != 3) // true
+
+
+// console.log("2"> 2) // true 
+// console.log("2" == 2) // true
+// console.log("2" === 2) // false
+
+// console.log(null == undefined) // true
+// console.log(null === undefined) // false
+// console.log(null > 0) // false
+// console.log(undefined == 0) // false
