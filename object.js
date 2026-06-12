b11672d2fd7d7e035e66d3dc4e0e6a084c434f88
Aman yadav <amanku12090@gmail.com>
2026-06-12 20:04:53 +0530

    Add object.js with singleton and object literal examples

---
+++ object.js
@@ -0,0 +1,38 @@
+// singleton
+
+// object literal
+
+// const mySym = Symbol("mySymbol")
+
+// const jsUser = {
+//     name: "Aman",
+//     age: 18,
+//     email: "aman@example.com",
+//     location: "India",
+//     isLoggedIn: true,
+//     lastLoginDays: ["Monday", "Tuesday", "Wednesday"],
+//     [mySym]: "myValue",
+// }
+
+// console.log(jsUser)
+// console.log(jsUser.name)
+// console.log(jsUser["lastLoginDays"])
+
+//  console.log(jsUser[mySym])
+
+//  jsUser.email = "amanyadav@example.com"
+//  console.log(jsUser.email)
+//   Object.freeze(jsUser)                                 // freeze the object to prevent modification
+//     jsUser.email = "amannkumar@example.com"
+//     console.log(jsUser)                                // email will not change due to freeze
+
+//  jsUser.greeting = function() {
+//    console.log("Hello js User!")
+// }
+
+//  jsUser.greeting2 = function() {
+//     console.log(`Hello js User, your name is ${this.name} and you are ${this.age} years old.`)
+//  }
+// console.log(jsUser.greeting());
+
+//  console.log(jsUser.greeting2());
